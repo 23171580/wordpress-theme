2791e2a7c0fc04e54c16b0be5ba4bf2e200d46d8
sun521xiaolei <sun521xiaolei@gmail.com>
2015-06-22 12:08:39 +0800

    upload

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<input type="text" name="s" id="s" placeholder="关键词" >
+            <input type="submit"  value="">
+</form>
